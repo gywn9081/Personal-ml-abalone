21f089c1e2ee3386844c8547ee14ce3d6f90765e
Henry Bloch <henry.bloch.04@gmail.com>
2023-05-11 09:14:44 -0500

    added jupyter notebooks

--- first_look.py
+++ first_look.py
@@ -10,6 +10,25 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import RepeatedKFold
 
+import matplotlib.pyplot as plt
+
+def z_score_dataframe(data_frame):
+    z_scored_data = (data_frame - data_frame.mean())/data_frame.std()
+    return(z_scored_data)
+
+
+
+
+
 data = pd.read_csv('./abalone-data/abalone.data', header=None, names=['sex', 'length', 'diameter', 'height', 'whole weight', 'shucked weight', 'viscera weight', 'shell weight', 'rings'])
 
-print(data)
+meta_dataset = data['sex']
+
+zscore = z_score_dataframe(data.drop(['sex'], axis=1))
+
+
+
+
+
+
+
